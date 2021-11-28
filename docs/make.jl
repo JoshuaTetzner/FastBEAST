b7cee5b15c94a13d7b2964b30f108b7bc62231fc
Joshua Tetzner <joshua.tetzner@igp.fraunhofer.de>
2021-11-28 12:31:20 +0100

    Documentation

--- docs/make.jl
+++ docs/make.jl
@@ -2,13 +2,13 @@ using FastBEAST
 using Pkg
 Pkg.add("Documenter")
 using Documenter
-##
+
 makedocs(
          sitename = "FastBEAST.jl",
          modules  = [FastBEAST],
          pages=[
                 "Home" => "index.md"
                ])
-deploydocs(;
-    repo="github.com/JoshuaTetzner/FastBEAST",
+deploydocs(
+    repo="https://github.com/JoshuaTetzner/FastBEAST",
 )
