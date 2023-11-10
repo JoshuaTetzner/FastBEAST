76af2427e54baae54eed2626cf30428361209547
Joshua <joshua.tetzner@uni-rostock.de>
2023-11-10 17:54:46 +0100

    Bugfix doc

--- docs/make.jl
+++ docs/make.jl
@@ -1,22 +1,43 @@
-using FastBEAST
-using Pkg
-Pkg.add("Documenter")
-using Documenter
+using Documenter, FastBEAST
 
 makedocs(
-         sitename = "FastBEAST.jl",
-         modules  = [FastBEAST],
-         pages=[
-                "Home" => "index.md",
-                "Getting Started" => "gstarted.md",
-                "Manual" => Any[
-                    "man/clustering.md",
-                    "man/hmatrix.md",
-                    "man/aca.md"
-                    ],
-                "Types and Functions" => "functions.md"
-               ])
+    sitename = "FastBEAST.jl",
+    modules = [FastBEAST],
+    pages = [
+        "Home" => "index.md",
+        "Getting Started" => "gstarted.md",
+        "Manual" => Any[
+            "man/clustering.md",
+            "man/hmatrix.md",
+            "man/aca.md"
+        ],
+        "Types and Functions" => "functions.md"
+])
+
 deploydocs(
-    repo="github.com/sbadrian/FastBEAST",
-    devbranch = "main"
+    repo = "github.com/JoshuaTetzner/FastBEAST.git",
+    devbranch = "bugfix/documentation",
+    versions = nothing,
 )
+
+#using Pkg
+#Pkg.add("Documenter")
+#using Documenter
+#
+#makedocs(
+#         sitename = "FastBEAST.jl",
+#         modules  = [FastBEAST],
+#         pages=[
+#                "Home" => "index.md",
+#                "Getting Started" => "gstarted.md",
+#                "Manual" => Any[
+#                    "man/clustering.md",
+#                    "man/hmatrix.md",
+#                    "man/aca.md"
+#                    ],
+#                "Types and Functions" => "functions.md"
+#               ])
+#deploydocs(
+#    repo="github.com/sbadrian/FastBEAST",
+#    devbranch = "main"
+#)
