28595b8b56b4ae608c496c0707c3b83b8991abc9
Joshua <joshua.tetzner@uni-rostock.de>
2023-11-23 10:30:05 +0100

    BEASTv2.1.0

--- docs/make.jl
+++ docs/make.jl
@@ -19,25 +19,3 @@ deploydocs(
     devbranch = "bugfix/documantation",
     versions = nothing,
 )
-
-#using Pkg
-#Pkg.add("Documenter")
-#using Documenter
-#
-#makedocs(
-#         sitename = "FastBEAST.jl",
-#         modules  = [FastBEAST],
-#         pages=[
-#                "Home" => "index.md",
-#                "Getting Started" => "gstarted.md",
-#                "Manual" => Any[
-#                    "man/clustering.md",
-#                    "man/hmatrix.md",
-#                    "man/aca.md"
-#                    ],
-#                "Types and Functions" => "functions.md"
-#               ])
-#deploydocs(
-#    repo="github.com/sbadrian/FastBEAST",
-#    devbranch = "main"
-#)
